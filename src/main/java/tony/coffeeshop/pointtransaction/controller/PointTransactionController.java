e5c6d2bb41897370cb258a13b9de0ecba1c8ecc4
ktony <k.tony9412@gmail.com>
2023-12-25 21:21:12 +0900

    feat: swagger apply on each controller

--- src/main/java/tony/coffeeshop/pointtransaction/controller/PointTransactionController.java
+++ src/main/java/tony/coffeeshop/pointtransaction/controller/PointTransactionController.java
@@ -1,8 +1,22 @@
 package tony.coffeeshop.pointtransaction.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import tony.coffeeshop.pointtransaction.domain.dto.PointDepositRequestDto;
+import tony.coffeeshop.pointtransaction.service.PointTransactionService;
 
 @RestController
+@RequiredArgsConstructor
 public class PointTransactionController {
 
+    private final PointTransactionService pointTransactionService;
+
+    @Operation(summary = "user deposit point")
+    @PostMapping("/point/deposit")
+    public int getAllMenu(@RequestBody PointDepositRequestDto pointDepositRequestDto) {
+        return pointTransactionService.depositPoint(pointDepositRequestDto);
+    }
 }
