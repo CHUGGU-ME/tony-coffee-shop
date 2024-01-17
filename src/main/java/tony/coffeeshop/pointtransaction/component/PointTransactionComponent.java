aab7496234f9042daecfa3e39019cfe361d665af
ktony <k.tony9412@gmail.com>
2024-01-17 20:34:52 +0900

    fix: Component layer get a lock by using redisson

--- src/main/java/tony/coffeeshop/pointtransaction/component/PointTransactionComponent.java
+++ src/main/java/tony/coffeeshop/pointtransaction/component/PointTransactionComponent.java
@@ -1,8 +1,12 @@
 package tony.coffeeshop.pointtransaction.component;
 
+import static tony.coffeeshop.pointtransaction.service.PointTransactionServiceImpl.USER_POINT_LOCK_PREFIX;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import tony.coffeeshop.common.LockHandler;
+import tony.coffeeshop.common.TransactionHandler;
 import tony.coffeeshop.pointtransaction.domain.PointTransaction;
 import tony.coffeeshop.pointtransaction.domain.dto.PointDepositRequestDto;
 import tony.coffeeshop.pointtransaction.repository.PointTransactionRepository;
@@ -16,13 +20,23 @@ public class PointTransactionComponent {
     private final PointTransactionRepository pointTransactionRepository;
     private final UserRepository userRepository;
 
-    @Transactional
-    public int depositPoint(PointDepositRequestDto pointDepositRequestDto) {
-        User user = userRepository.findById(pointDepositRequestDto.getUserSeq()).get();
-        user.depositPoint(pointDepositRequestDto.getPoint());
+    private final LockHandler lockHandler;
+    private final TransactionHandler transactionHandler;
 
-        PointTransaction pointTransaction = pointDepositRequestDto.toEntity(user);
-        pointTransactionRepository.save(pointTransaction);
-        return user.getUserPoint();
+    public void depositPoint(PointDepositRequestDto pointDepositRequestDto) {
+        lockHandler.runOnLock(
+                USER_POINT_LOCK_PREFIX + pointDepositRequestDto.getUserSeq(),
+                2000L,
+                1000L,
+                () -> transactionHandler.runOnWriteTransaction(
+                        () -> {
+
+                            User user = userRepository.findById(pointDepositRequestDto.getUserSeq()).get();
+                            user.depositPoint(pointDepositRequestDto.getPoint());
+
+                            PointTransaction pointTransaction = pointDepositRequestDto.toEntity(user);
+                            pointTransactionRepository.save(pointTransaction);
+                            return null;
+                        }));
     }
 }
