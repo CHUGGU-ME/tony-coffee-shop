c3bd25f2c612c0bb5748ece88710bbe55547216f
ktony <k.tony9412@gmail.com>
2024-01-27 23:47:05 +0900

    fix: Working on concurrency order handling

--- src/test/java/tony/coffeeshop/order/service/OrderServiceImplTest.java
+++ src/test/java/tony/coffeeshop/order/service/OrderServiceImplTest.java
@@ -1,21 +1,18 @@
 package tony.coffeeshop.order.service;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import jakarta.persistence.EntityManager;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 import tony.coffeeshop.menu.domain.Menu;
-import tony.coffeeshop.menu.domain.dto.MenuResponseDto;
 import tony.coffeeshop.menu.repository.MenuRepository;
 import tony.coffeeshop.menu.service.MenuService;
-import tony.coffeeshop.order.domain.Order;
 import tony.coffeeshop.order.domain.dto.OrderRequestDto;
 import tony.coffeeshop.order.domain.dto.OrderResponseDto;
 import tony.coffeeshop.order.domain.dto.OrderWeeklyTop3;
@@ -23,7 +20,7 @@ import tony.coffeeshop.user.domain.User;
 import tony.coffeeshop.user.repository.UserRepository;
 
 @SpringBootTest
-@Transactional
+@Slf4j
 class OrderServiceImplTest {
 
     @Autowired
@@ -37,6 +34,9 @@ class OrderServiceImplTest {
 
     @Autowired
     private MenuService menuService;
+
+    @Autowired
+    EntityManager em;
 
     @DisplayName("user orders menu by using its point")
     @Test
@@ -79,6 +79,78 @@ class OrderServiceImplTest {
         Assertions.assertThat(orderResponseDto.getUserSeq()).isEqualTo(saveUser.getId());
         Assertions.assertThat(orderResponseDto.getMenuName()).isEqualTo(hotChoco.getMenuName());
         Assertions.assertThat(orderResponseDto.getOrderPrice()).isEqualTo(hotChoco.getMenuPrice());
+    }
+
+    @DisplayName("user orders menu concurrently")
+    @Test
+    public void userOrderMenuConcurrently() throws InterruptedException {
+        // given
+        User testUser = User.builder()
+                .userId("testUser")
+                .userPoint(10000)
+                .build();
+
+        User saveUser = userRepository.save(testUser);
+
+        Menu iceAmericano = Menu.builder()
+                .menuName("ice americano")
+                .menuPrice(5000)
+                .build();
+        Menu hotChoco = Menu.builder()
+                .menuName("hot chocolate")
+                .menuPrice(4500)
+                .build();
+        Menu hotLatte = Menu.builder()
+                .menuName("hotLatte")
+                .menuPrice(5500)
+                .build();
+        menuRepository.saveAll(List.of(iceAmericano, hotChoco, hotLatte));
+
+        // when
+        OrderRequestDto orderRequestDto1 = OrderRequestDto.builder()
+                .userSeq(saveUser.getId())
+                .menuId(hotChoco.getId())
+                .orderedAt(LocalDateTime.now())
+                .build();
+
+        OrderRequestDto orderRequestDto2 = OrderRequestDto.builder()
+                .userSeq(saveUser.getId())
+                .menuId(hotLatte.getId())
+                .orderedAt(LocalDateTime.now())
+                .build();
+
+        CompletableFuture.allOf(
+                CompletableFuture.runAsync(() -> orderService.orderMenu(orderRequestDto1)),
+                CompletableFuture.runAsync(() -> orderService.orderMenu(orderRequestDto2))
+        ).join();
+
+
+        /*
+        List<OrderRequestDto> orders = List.of(orderRequestDto1, orderRequestDto2);
+        int numberThreads = 2;
+        CountDownLatch countDownLatch = new CountDownLatch(numberThreads);
+        ExecutorService executorService = Executors.newFixedThreadPool(numberThreads);
+
+        for (int i = 0; i < numberThreads; i++) {
+            int idx = i;
+            try {
+                executorService.execute(
+                        () -> orderService.orderMenu(orders.get(idx))
+                );
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                countDownLatch.countDown();
+            }
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        */
+
+        // then
+        User user = userRepository.findById(orderRequestDto1.getUserSeq()).get();
+        int userPoint = user.getUserPoint();
+        Assertions.assertThat(10000 - 5500 - 5500).isEqualTo(userPoint);
     }
 
     @DisplayName("search weekly top 3 menus")
