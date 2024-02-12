5c76416f3801c8a1bf716cabde1b1ace7d91c172
ktony <k.tony9412@gmail.com>
2024-02-12 21:05:33 +0900

    fix: add @RequestMapping on menu controller, @Getter add on MenuResponseDto

--- src/main/java/tony/coffeeshop/menu/controller/MenuController.java
+++ src/main/java/tony/coffeeshop/menu/controller/MenuController.java
@@ -1,23 +1,24 @@
 package tony.coffeeshop.menu.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tony.coffeeshop.menu.domain.dto.MenuResponseDto;
 import tony.coffeeshop.menu.service.MenuService;
 
 @RestController
+@RequestMapping("/menu")
 @RequiredArgsConstructor
 public class MenuController {
 
     private final MenuService menuService;
 
-    @Operation(summary = "get all menu")
     @GetMapping("/all-menu")
-    public List<MenuResponseDto> getAllMenu() {
-        return menuService.getAllMenu();
+    public ResponseEntity<List<MenuResponseDto>> getAllMenu() {
+        return ResponseEntity.ok(menuService.getAllMenu());
     }
 
 }
--- src/main/java/tony/coffeeshop/menu/domain/dto/MenuResponseDto.java
+++ src/main/java/tony/coffeeshop/menu/domain/dto/MenuResponseDto.java
@@ -1,14 +1,18 @@
 package tony.coffeeshop.menu.domain.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Setter
+@Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MenuResponseDto {
+
     private Long id;
-
     private String menuName;
-
     private int menuPrice;
 }
