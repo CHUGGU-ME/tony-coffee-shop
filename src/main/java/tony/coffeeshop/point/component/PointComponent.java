f3a1355cc64e6a1656f157a34eb1f11590439ae7
ktony <k.tony9412@gmail.com>
2024-02-18 22:59:55 +0900

    refactor: PointComponent is followed by point domain.

--- src/main/java/tony/coffeeshop/point/component/PointComponent.java
+++ src/main/java/tony/coffeeshop/point/component/PointComponent.java
@@ -1,23 +1,22 @@
-package tony.coffeeshop.pointtransaction.component;
+package tony.coffeeshop.point.component;
 
-import static tony.coffeeshop.pointtransaction.service.PointTransactionServiceImpl.USER_POINT_LOCK_PREFIX;
+import static tony.coffeeshop.point.service.PointServiceImpl.USER_POINT_LOCK_PREFIX;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 import tony.coffeeshop.common.LockHandler;
 import tony.coffeeshop.common.TransactionHandler;
-import tony.coffeeshop.pointtransaction.domain.PointTransaction;
-import tony.coffeeshop.pointtransaction.domain.dto.PointDepositRequestDto;
-import tony.coffeeshop.pointtransaction.repository.PointTransactionRepository;
+import tony.coffeeshop.point.domain.Point;
+import tony.coffeeshop.point.domain.dto.PointDepositRequestDto;
+import tony.coffeeshop.point.repository.PointRepository;
 import tony.coffeeshop.user.domain.User;
 import tony.coffeeshop.user.repository.UserRepository;
 
 @Component
 @RequiredArgsConstructor
-public class PointTransactionComponent {
+public class PointComponent {
 
-    private final PointTransactionRepository pointTransactionRepository;
+    private final PointRepository pointRepository;
     private final UserRepository userRepository;
 
     private final LockHandler lockHandler;
@@ -34,8 +33,8 @@ public class PointTransactionComponent {
                             User user = userRepository.findById(pointDepositRequestDto.getUserSeq()).get();
                             user.depositPoint(pointDepositRequestDto.getPoint());
 
-                            PointTransaction pointTransaction = pointDepositRequestDto.toEntity(user);
-                            pointTransactionRepository.save(pointTransaction);
+                            Point point = pointDepositRequestDto.toEntity(user);
+                            pointRepository.save(point);
                             return null;
                         }));
     }
